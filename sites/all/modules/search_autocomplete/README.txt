755a1b7b0cc095450333fe289f728da563bcef32
Vince Tingey <vincetingey@gmail.com>
2014-04-23 15:47:55 -0700

    Updated Search Autocomplete Module to v7.x-3.3 https://drupal.org/project/search_autocomplete Used in old CLF theme at top of page. Tested ok.

--- sites/all/modules/search_autocomplete/README.txt
+++ sites/all/modules/search_autocomplete/README.txt
@@ -31,7 +31,7 @@ files.
 --------------------------------------------------------------------------------- 
 -- 2.  Updating Search Autocomplete:
 
-BEWARE: Update to 7.x-3.x will lose every previous configuration. Please make
+BEWARE: Update to 7.x-3.x from 7.x-2.x will lose every previous configuration. Please make
 sure you can produce the suggestion set you need using views. This should be 
 easy !
 
@@ -41,13 +41,19 @@ or in the equivalent directory of your Drupal installation.
 Place all files from this 3.x version in this sites/all/modules/search_autocomplete
 or equivalent folder.
 
-Run the update script. Make sure to run the udpate functions.
+Run the update script. Make sure to run the update functions.
 
 If you're having trouble installing this module, please ensure that your tar 
 program is not flattening the directory tree, truncating filenames or losing 
 files.
 You can also perform a full and complete uninstall of Search Autocomplete and try to
 re-install.
+
+Another solution would be to refer to the documentation: 
+http://projects.axiomcafe.fr/search-autocomplete
+
+Finally, you can also post an issue at:
+https://drupal.org/project/issues/search_autocomplete
 
 ---------------------------------------------------------------------------------
 -- 3.  Setting Search Autocomplete
@@ -56,7 +62,7 @@ Navigate to /admin/config/search/search_autocomplete
 
 The configuration options are not as quite easy to understand as it was in previous
 version. Please consider reading the documentation available at:
-http://projects.axiomcafe.fr
+http://projects.axiomcafe.fr/search-autocomplete
 
 ---------------------------------------------------------------------------------
 -- 4.  Translating Search Autocomplete
@@ -64,7 +70,7 @@ http://projects.axiomcafe.fr
 Please visit the module translation page to download translation:
 http://localize.drupal.org/translate/downloads?project=search_autocomplete.
 
-Please refer to section 5 (Helping) for typo, grammar or langage issues.
+Please refer to section 5 (Helping) for typo, grammar or language issues.
 
 ---------------------------------------------------------------------------------
 -- 5.  Helping and complaining on Search Autocomplete
@@ -72,7 +78,7 @@ Please refer to section 5 (Helping) for typo, grammar or langage issues.
 To help this module live, please post your issues, ideas and comments at:
 http://drupal.org/node/add/project-issue/search_autocomplete
 and view issues at:
-http://drupal.org/project/issues/search_autocomplete?categories=All
+http://drupal.org/project/issues/search_autocomplete
 
 
-The maintener: Miroslav
+The maintainer: Miroslav
--- sites/all/modules/search_autocomplete/js/jquery.autocomplete.js
+++ sites/all/modules/search_autocomplete/js/jquery.autocomplete.js
@@ -1,6 +1,9 @@
-/*
+/**
+ * @file
  * SEARCH AUTOCOMPLETE (version 7.3-x)
- *
+ */
+ 
+/*
  * @authors
  * Miroslav Talenberg (Dominique CLAUSE) <http://www.axiomcafe.fr/contact>
  *
@@ -13,21 +16,31 @@
     attach: function(context) {
       if (Drupal.settings.search_autocomplete) {
         $.each(Drupal.settings.search_autocomplete, function(key, value) {
+          var NoResultsLabel = Drupal.settings.search_autocomplete[key].no_results;
           $(Drupal.settings.search_autocomplete[key].selector).addClass('ui-autocomplete-processed').autocomplete({
             minLength: Drupal.settings.search_autocomplete[key].minChars,
             source: function(request, response) {
               if (Drupal.settings.search_autocomplete[key].type == 0) {             // external URL
                 $.getJSON(Drupal.settings.search_autocomplete[key].datas, { q: request.term }, function (results) {
                   // Only return the number of values set in the settings.
+                  if (!results.length && NoResultsLabel) {
+                      results = [NoResultsLabel];
+                  }   
                   response(results.slice(0, Drupal.settings.search_autocomplete[key].max_sug));
                 });
               } else if (Drupal.settings.search_autocomplete[key].type == 1) {      // internal URL
-                $.getJSON(Drupal.settings.search_autocomplete[key].datas + '/' + request.term, { }, function (results) {
+                $.getJSON(Drupal.settings.search_autocomplete[key].datas + request.term, { }, function (results) {
                   // Only return the number of values set in the settings.
+                  if (!results.length && NoResultsLabel) {
+                      results = [NoResultsLabel];
+                  }
                   response(results.slice(0, Drupal.settings.search_autocomplete[key].max_sug));
                 });
-              } else if (Drupal.settings.search_autocomplete[key].type == 2) {      // static ressources
+              } else if (Drupal.settings.search_autocomplete[key].type == 2) {      // static resources
                 var results = $.ui.autocomplete.filter( Drupal.settings.search_autocomplete[key].datas, request.term );
+                    if (!results.length && NoResultsLabel) {
+                    results = [NoResultsLabel];
+                }
                 // Only return the number of values set in the settings.
                 response(results.slice(0, Drupal.settings.search_autocomplete[key].max_sug));
               }
@@ -37,11 +50,19 @@
               $(".ui-autocomplete li.ui-menu-item:even").addClass("ui-menu-item-even");
             },
             select: function(event, ui) { 
+              if (ui.item.label === NoResultsLabel) {
+                event.preventDefault();
+              } else
               if (Drupal.settings.search_autocomplete[key].auto_redirect == 1 && ui.item.link) {
                 document.location.href = ui.item.link;
               } else if (Drupal.settings.search_autocomplete[key].auto_submit == 1) {
                   $(this).val(ui.item.label);
                   $(this).closest("form").submit();
+              }
+            },
+            focus: function (event, ui) {
+              if (ui.item.label === NoResultsLabel) {
+                  event.preventDefault();
               }
             }
           }).autocomplete("widget").attr("id", "ui-theme-" + Drupal.settings.search_autocomplete[key].theme);
